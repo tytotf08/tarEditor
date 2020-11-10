dca1485c2cfd661b674314e7148a6193e12aca6b
tytotf08 <joshleeliu@gmail.com>
2020-11-10 08:39:04 -0500

    WebEditor Version 1.1.1 !

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tar Editor 
+
+Tar Editor is an in-development text editor with an Electron engine, currently, being built, and a web technologies frontend. The frontend doesn't utilize any external libraries except for highlight.js, and the core editor is built entirely from scratch.
+
+## About the Editor
+
+Tar Editor's editor is made with the intention of being functional, powerful and fast. It uses one editable HTML ```<div>``` and looks to keep it's Document Object Model simple and short.
--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,23 @@
-"use strict";
-let savedRange,isInFocus;
-function saveCaretPosition(context){
-	var selection = window.getSelection();
-	var range = selection.getRangeAt(0);
-	range.setStart(  context, 0 );
-	var len = range.toString().length;
-
-	return function restore(){
-		var pos = getTextNodeAtPosition(context, len);
-		selection.removeAllRanges();
-		var range = new Range();
-		range.setStart(pos.node ,pos.position);
-		selection.addRange(range);
-
-	}
+"use strict"; 
+const editor = document.querySelector("div#editor");
+let savedRange, isInFocus, text, restore, prev, selection, range, len, pos, s, r0, r, splice, endOfLine, currentLine, tabs, index;
+const debounce = function(cb, wait) {
+	let timeout = 0;
+	return (...args) => {
+		clearTimeout(timeout);
+		timeout = window.setTimeout(() => cb(...args), wait);
+	};
 }
-function beforeCursor(context) {
-  const s = window.getSelection();
-  const r0 = s.getRangeAt(0);
-  const r = document.createRange();
-  r.selectNodeContents(context);
-  r.setEnd(r0.startContainer, r0.startOffset);
-  return r.toString();
+const insertText = function(text) {
+	text = text
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
+	document.execCommand("insertHTML", false, text);
 }
-function getLeadingTabs(context) {
-	var splice = beforeCursor(context);
-	var endOfLine = splice.lastIndexOf("\n");
-
-	var currentLine = splice.substr(endOfLine+1);
-
-	var tabs = "";
-  var index = 0;
-  while (currentLine.charAt(index++) === "\t") {
-    tabs += "\t";
-  }
-  if (currentLine.trim().endsWith("{")) {
-  	tabs += "\t";
-  }
-  return tabs;
-}
-
-function getTextNodeAtPosition(root, index){
+const getTextNodeAtPosition = function(root, index){
 	const NODE_TYPE = NodeFilter.SHOW_TEXT;
 	var treeWalker = document.createTreeWalker(root, NODE_TYPE, function next(elem) {
 		if(index > elem.textContent.length){
@@ -55,25 +32,84 @@ function getTextNodeAtPosition(root, index){
 		position: index
 	};
 }
-const editor = document.querySelector("div#editor");
-editor.contentEditable = true;
-editor.addEventListener("input", function(e) {
-	e.stopPropagation();
-	let restore = saveCaretPosition(this);
+const saveCaretPosition = function(context){
+	selection = window.getSelection();
+	range = selection.getRangeAt(0);
+	range.setStart(context, 0);
+	len = range.toString().length;
+
+	return function restore(){
+		pos = getTextNodeAtPosition(context, len);
+		selection.removeAllRanges();
+		range = new Range();
+		range.setStart(pos.node ,pos.position);
+		selection.addRange(range);
+	}
+}
+const beforeCursor = function(context) {
+	s = window.getSelection();
+	r0 = s.getRangeAt(0);
+	r = document.createRange();
+	r.selectNodeContents(context);
+	r.setEnd(r0.startContainer, r0.startOffset);
+	return r.toString();
+}
+const getLeadingTabs = function(context) {
+	splice = beforeCursor(context);
+	endOfLine = splice.lastIndexOf("\n");
+
+	currentLine = splice.substr(endOfLine+1);
+
+	tabs = "";
+	index = 0;
+	while (currentLine.charAt(index++) === "\t") {
+		tabs += "\t";
+	}
+	if (currentLine.trim().endsWith("{")) {
+		tabs += "\t";
+	}
+	return tabs;
+}
+const highlight = function() {
+	restore = saveCaretPosition(editor);
 	editor.textContent = editor.textContent;
-  hljs.highlightBlock(editor);
+	hljs.highlightBlock(editor);
 	restore();
-});
+}
+const waitToHighlight = debounce(() => {
+	highlight();
+}, 30);
+editor.setAttribute("contentEditable", "plaintext-only");
 editor.addEventListener("keydown", function(e) {
-	if (e.keyCode === 13) {
-		var tabs = getLeadingTabs(editor);
-		document.execCommand("insertHTML", false, "\n"+tabs);
+	prev = editor.textContent;
+	if (e.key === "Enter") {
+		tabs = getLeadingTabs(editor);
+		if (tabs.length > 0) {
+			e.preventDefault();
+			insertText("\n"+tabs);
+		}
 	}
-	if (e.keyCode === 9) {
+	if (e.key === "Tab") {
 		e.preventDefault();
-		document.execCommand("insertHTML", false, "\t");
+		insertText("\t");
 	}
+});
+editor.addEventListener("keyup", function(e) {
+	if (prev !== editor.textContent){
+		waitToHighlight();
+	}
+});
+editor.addEventListener("paste", function(e) {
+	e.preventDefault();
+	text = (event.originalEvent || event).clipboardData.getData("text/plain");
+	insertText(text);
+	restore = saveCaretPosition(editor);
+	hljs.highlightBlock(editor);
+	restore();
 });
 document.addEventListener("DOMContentLoaded", function(e) {
 	hljs.highlightBlock(editor);
+});
+window.addEventListener("load", function(e) {
+	editor.focus();
 });
