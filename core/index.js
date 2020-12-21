c926503d57beed9a4f72c8c774bc11c5255df73f
tytotf08 <joshleeliu@gmail.com>
2020-12-21 13:28:19 -0500

    Add line numbers and undo redo.

--- core/index.js
+++ core/index.js
@@ -1,212 +1,14 @@
-// "use strict";
-// const initScroller = scroller => { // this.scroller styles
-// 	scroller.style.overflowY = "auto";
-// 	scroller.classList.add("tar-scroller");
-// 	scroller.style.display = "flex";
-// 	scroller.style.flexDirection = "center";
-// };
-// const initLineNumbers = lineNumbers => {
-// 	lineNumbers.style.width = "3%";
-// 	lineNumbers.innerHTML = "1<br>";
-// 	lineNumbers.style.paddingTop = "8px";
-// 	lineNumbers.style.display = "flex";
-// 	lineNumbers.style.flexDirection = "column";
-// 	lineNumbers.style.alignItems = "center";
-// };
-// const initWrap = wrap => { // wrap styles
-// 	wrap.classList.add("tar-wrap"); 
-// 	wrap.style.display = "flex";
-// 	wrap.style.flexDirection = "column";
-// 	wrap.style.overflow = "hidden";
-// };
-// const initTextarea = (textarea, ln = false) => { // set textarea styles
-// 	textarea.setAttribute("contenteditable", window.navigator.userAgent.toLowerCase().indexOf("firefox") > -1 ? true: "plaintext-only");
-// 	textarea.style.outline = "0px solid transparent";
-// 	textarea.style.whiteSpace = "pre";
-// 	textarea.style.wordWrap = "normal";
-// 	textarea.setAttribute("spellcheck", false);
-// 	textarea.classList.add("tar-textarea");
-// 	textarea.style.padding = "8px";
-// };
-// const beforeCursor = (el) => { // get all text before caret
-// 	const s = window.getSelection();
-// 	const r0 = s.getRangeAt(0);
-// 	const r = document.createRange();
-// 	r.selectNodeContents(el);
-// 	r.setEnd(r0.startContainer, r0.startOffset);
-// 	return r.toString();
-// };
-// const afterCursor = (el) => { // get all text after caret
-// 	const s = window.getSelection();
-// 	const r0 = s.getRangeAt(0);
-// 	const r = document.createRange();
-// 	r.selectNodeContents(el);
-// 	r.setStart(r0.endContainer, r0.endOffset);
-// 	return r.toString();
-// };
-// const restoreCaretPosition = (pos, el) => { // set caret
-// 	for (const node of el.childNodes) {
-// 		if (node.nodeType == Node.TEXT_NODE) {
-// 			if (node.length >= pos) {
-// 				let range = document.createRange();
-// 				let sel = window.getSelection();
-// 				range.setStart(node, pos);
-// 				range.collapse(true);
-// 				sel.removeAllRanges();
-// 				sel.addRange(range);
-// 				return -1;
-// 			} else {
-// 				pos = pos - node.length;
-// 			}
-// 		} else {
-// 			pos = restoreCaretPosition(pos, node);
-// 			if (pos < 0) {
-// 				return pos;
-// 			}
-// 		}
-// 	}
-// 	return pos;
-// };
-// class Tar { // main class 
-// 	constructor(wrap) { // get wrap this.element
-// 		if (!(wrap instanceof Node)) { // is wrap an this.element 
-// 			this.success = false;
-// 		} else {
-// 			this.success = true;
-// 		};
-// 		let ln_content = "1<br>"; // what to put in line numbers this.el (if existing) 
-// 		this.ln = false; // ln boolean accessible in object scope 
-// 		this.scroller = document.createElement("DIV"); // this.scroller 
-// 		this.el = document.createElement("DIV"); // textarea 
-// 		let prev = this.el.textContent; // before
-// 		const save = () => {
-// 			return beforeCursor(this.el).length; // return the length of the text before caret 
-// 		};
-// 		const editor = { // special object
-// 			html: () => {
-// 				return this.el.innerHTML; // get displayed html 
-// 			},
-// 			plain: () => {
-// 				return this.el.textContent; // get plain textcontent 
-// 			},
-// 			on: (e, fn) => {
-// 				for (let i = 0; i < e.length; i++) { // takes an array so one callback can be for multiple events 
-// 					this.el.addEventListener(e[i], fn);
-// 				};			
-// 			},
-// 			insert: (text) => {
-// 				let before = beforeCursor(this.el); // get before
-// 				const pos = save(); // get pos
-// 				const after = afterCursor(this.el); // get after
-// 				before += text; // add text to before
-// 				this.el.textContent = before + after; // set html as the new text
-// 				editor.setCaret(pos+text.length); // restore caret after text
-// 			},
-// 			setHTML: (text) => {
-// 				this.el.innerHTML = text;
-// 			},
-// 			setCaret: (pos) => {
-// 				restoreCaretPosition(pos, this.el);
-// 			},
-// 			diff: (cb) => {
-// 				if (prev !== this.el.textContent) {
-// 					cb();
-// 				}
-// 			}
-// 		};
-// 		const indent = e => {
-// 			if (e.key === "Tab") {
-// 				e.preventDefault();
-// 				if (e.shiftKey) {
-// 				} else {
-// 					const sel = String(window.getSelection()); // get selection
-// 					editor.insert("\t" + sel); // insert tab and selection
-// 					const before = beforeCursor(this.el); // get before caret
-// 					editor.setCaret(before.length-sel.length); // restore after tab
-// 				}
-// 				return true; // key is tab
-// 			} else {
-// 				return false; // other key
-// 			}
-// 		};
-// 		const lastOf = (a) => {
-// 			return a[a.length-1]; // length starts at 1 so subtract 1 to get last this.element in array
-// 		};
-// 		const enter = e => {
-// 			if (e.key === "Enter") {
-// 				const currentLine = lastOf(beforeCursor(this.el).split("\n")).split("");
-// 				let tabs = "";
-// 				for (let i = 0; i < currentLine.length; i++) {
-// 					if (currentLine[i] === "\t") {
-// 						tabs += "\t";
-// 					}
-// 				};
-// 				if (tabs.length > 0 || (window.navigator.userAgent.toLowerCase().indexOf("firefox") > -1)) {
-// 					e.preventDefault();
-// 					editor.insert("\n" + tabs);
-// 				}
-// 				return true; // key is enter
-// 			} else {
-// 				return false; // other key
-// 			}
-// 		};
-// 		editor.on(["keydown"], e => {
-// 			prev = editor.plain();
-// 			const isEnter = enter(e);
-// 			if (isEnter && this.ln === true) {
-// 				if (this.lineNumbers) {
-// 					const splitLineNumbers = this.lineNumbers.innerHTML.split("<br>");
-// 					splitLineNumbers.pop();
-// 					const numLast = Number(lastOf(splitLineNumbers));
-// 					const nextNum = String(numLast + 1) + "<br>";
-// 					this.lineNumbers.innerHTML += nextNum;
-// 				} else {
-// 					this.lineNumbers = document.createElement("DIV");
-// 					initTextarea(this.el, true);
-// 					initLineNumbers(this.lineNumbers);
-// 					this.scroller.insertBefore(this.lineNumbers, this.el);
-// 				}
-// 			}
-// 			const isTab = indent(e);
-// 		});
-// 		editor.on(["paste"], e => {
-// 			e.preventDefault();
-// 			const text = e.clipboardData.getData("text/plain");
-// 			editor.insert(text);
-// 		});
-// 		editor.on(["keyup"], e => {
-// 			editor.diff(() => {
-// 				const pos = save();
-// 				let text = editor.plain();
-// 				text = text
-// 					.replace(/</g, "&lt;")
-// 					.replace(/>/g, "&gt;");
-// 				editor.setHTML(text);
-// 				editor.setCaret(pos);
-// 			});
-// 		});
-// 		initScroller(this.scroller);
-// 		initWrap(wrap);
-// 		initTextarea(this.el, this.ln);
-// 		if (this.ln === true) {
-// 			this.lineNumbers = document.createElement("DIV");
-// 			initLineNumbers(this.lineNumbers);
-// 			this.scroller.insertBefore(this.lineNumbers, this.el);
-// 		}
-// 		this.scroller.appendChild(this.el);
-// 		wrap.appendChild(this.scroller);
-// 		return this;
-// 	}
-// 	setLN(b) {
-// 		if (typeof b === "boolean") {
-// 			this.ln = b;
-// 			this.lineNumbers = document.createElement("DIV");
-// 			initLineNumbers(this.lineNumbers);
-// 			this.scroller.insertBefore(this.lineNumbers, this.el);
-// 		}
-// 		return this;
-// 	}
-// };
+const specialChars = {
+	start: [
+		"\"", "'", "`", "{", "["
+	],
+	end: [
+		"\"", "'", "`", "}", "]"
+	]
+};
+const lastOf = a => {
+	return a[a.length-1];
+};
 const restoreCaretPosition = (pos, context) => {
 	for (const node of context.childNodes) {
 		if (node.nodeType == Node.TEXT_NODE) {
@@ -256,6 +58,8 @@ const initScroller = scroller => {
 	scroller.style.flex = 1;
 	scroller.classList.add("tar-scroller");
 	scroller.style.overflow = "auto";
+	scroller.style.display = "flex";
+	scroller.style.alignItems = "flex-start";
 };
 const initTextarea = textarea => {
 	textarea.setAttribute("contenteditable", navigator.userAgent.toLowerCase().indexOf("firefox") > -1 ? true : "plaintext-only");
@@ -266,19 +70,24 @@ const initTextarea = textarea => {
 	textarea.style.overflowY = "hidden";
 	textarea.style.padding = "6px";
 	textarea.style.outline = "0px solid transparent";
+	textarea.style.flex = 1;
 };
 class Tar {
 	constructor(wrap) {
 		if (!(wrap instanceof Node)) return false;
+		this.ln = false;
 		this.wrap = wrap;
 		this.scroller = document.createElement("DIV");
 		this.textarea = document.createElement("DIV");
 		initTextarea(this.textarea);
 		this.prev = this.textarea.textContent;
-		initScroller(this.scroller);
-		initWrap(this.wrap);
-		this.scroller.appendChild(this.textarea);
-		this.wrap.appendChild(this.scroller);
+		this.timeMachine = [
+			{
+				html: "",
+				pos: 0
+			}
+		];
+		this.at = 0;
 		this.editor = {
 			on: (e, f) => {
 				this.textarea.addEventListener(e, f);
@@ -291,13 +100,13 @@ class Tar {
 			},
 			diff: cb => {
 				if (typeof cb === "function") {
-					if (prev !== this.editor.textContent()) {
+					if (this.prev !== this.editor.textContent()) {
 						cb();
 					}
 				}
 			},
 			refresh: () => {
-				prev = this.editor.textContent();
+				this.prev = this.editor.textContent();
 			},
 			before: () => {
 				return beforeCursor(this.textarea);
@@ -316,14 +125,57 @@ class Tar {
 				const pos = this.editor.save();
 				this.textarea.textContent = newContent;
 				this.editor.restore(pos + text.length);
+			},
+			setHTML: (text = "") => {
+				this.textarea.innerHTML = text;
 			}
 		};
 		this.editor.on("keydown", e => {
+			this.editor.refresh();
 			const isTab = this.indent(e);
+			const isEnter = this.enter(e);
+			const isAutoComplete = this.autoComplete(e);
+			const undoRedo = this.setVer(e);
+			const isBackSpace = this.delete(e);
 		});
+		this.editor.on("keyup", e => {
+			this.editor.diff(() => {
+				this.lineNumbers.innerHTML = "1<br>";
+				const lines = this.editor.textContent().split(/\n(?!$)/g).length;
+				for (let i = 1; i < lines; i++) {
+					this.lineNumbers.innerHTML += String(i + 1) + "<br>";
+				}
+				const pos = this.editor.save();
+				const text = this.editor.textContent().replace(/</g, "&lt;");
+				this.editor.setHTML(text);
+				this.editor.restore(pos);
+				const splitText = this.editor.innerHTML().split("\n");
+				window.setTimeout(() => {
+					if (this.timeMachine[this.at].html === this.editor.innerHTML()) {
+						return;
+					}
+					this.at++;
+					const html = this.editor.innerHTML();
+					const pos = this.editor.save();
+					this.timeMachine.push({html, pos});
+				}, 300);
+			});
+		});
+		initScroller(this.scroller);
+		initWrap(this.wrap);
+		this.scroller.appendChild(this.textarea);
+		this.wrap.appendChild(this.scroller);
 		return this;
 	}
 	setLN(b) {
+		this.ln = true;
+		this.lineNumbers = document.createElement("DIV");
+		this.lineNumbers.style.width = "3%";
+		this.textarea.style.flex = "97%";
+		this.scroller.insertBefore(this.lineNumbers, this.textarea);
+		this.lineNumbers.innerHTML = "1<br>";
+		this.lineNumbers.style.paddingTop = "6px";
+		this.lineNumbers.style.textAlign = "center";
 		return this;
 	}
 	indent(e) {
@@ -341,4 +193,100 @@ class Tar {
 			return false;
 		}
 	}
-}
+	enter(e) {
+		if (e.key === "Enter") {
+			const currentLine = lastOf(this.editor.before().split("\n"));
+			let tabs = "";
+			let i = 0;
+			while (currentLine.charAt(i) === "\t") {
+				tabs += "\t";
+				i++;
+			};
+			if (currentLine.endsWith("{") && this.editor.after().startsWith("}")) {
+				e.preventDefault();
+				this.editor.insert("\n" + tabs + "\t");
+				const pos = this.editor.save();
+				this.editor.insert("\n" + tabs);
+				this.editor.restore(pos);
+			} else {
+				if (tabs.length > 0) {
+					e.preventDefault();
+					this.editor.insert("\n" + tabs);
+				}
+			}
+			if (this.lineNumbers) {
+				const next = String(Number(this.lineNumbers.innerHTML.split("<br>")[this.lineNumbers.innerHTML.split("<br>").length-2]) + 1) + "<br>";
+				this.lineNumbers.innerHTML += next;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	autoComplete(e) {
+		for (let i = 0; i < specialChars.start.length; i++) {
+			if (e.key === specialChars.start[i]) {
+				e.preventDefault();
+				if (specialChars.start[i] === specialChars.end[i] && ((this.editor.before().endsWith(specialChars.start[i]) && this.editor.after().startsWith(specialChars.end[i])))) {
+					const pos = this.editor.save() + 1;
+					this.editor.restore(pos);
+				} else {
+					this.editor.insert(specialChars.start[i] + specialChars.end[i]);
+					const pos = this.editor.save() - 1;
+					this.editor.restore(pos);
+				}
+				break;	
+			} else if (e.key === specialChars.end[i]) {
+				if (this.editor.before().endsWith(specialChars.start[i]) && this.editor.after().startsWith(specialChars.end[i])) {
+					e.preventDefault();
+					const pos = this.editor.save() + 1;
+					this.editor.restore(pos);
+				} 
+				break;
+			}
+		}
+	}
+	setVer(e) {
+		if (!e.shiftKey && (e.ctrlKey || e.metaKey) && e.key === "z") {
+			e.preventDefault();
+			this.at--;
+			if (this.at < 0) {
+				this.at = 0;
+			} else {
+				this.editor.setHTML(this.timeMachine[this.at].html);
+				this.editor.restore(this.timeMachine[this.at].pos);
+			}
+			return true;
+		} else {
+			if (e.shiftKey && (e.ctrlKey || e.metaKey) && e.key === "z") {
+				e.preventDefault();
+				this.at++;
+				if (this.at >= this.timeMachine.length) {
+					this.at = this.timeMachine.length-1;
+				} else {
+					this.editor.setHTML(this.timeMachine[this.at].html);
+					this.editor.restore(this.timeMachine[this.at].pos);
+				}
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	delete(e) {
+		if (e.key === "Backspace") {
+			const currentLine = lastOf(this.editor.innerHTML().split("\n"));
+			this.lineNumbers.innerHTML = "1<br>";
+			const lines = this.editor.textContent().split(/\n(?!$)/g).length;
+			for (let i = 2; i < lines; i++) {
+				this.lineNumbers.innerHTML += String(i) + "<br>";
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+};
+const tar = (wrap) => {
+	return new Tar(wrap);
+};
