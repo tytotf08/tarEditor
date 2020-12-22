53120310e49914e029d7c755e36cbfebabfb3572
tytotf08 <joshleeliu@gmail.com>
2020-12-22 14:07:37 -0500

    Remove having to do as.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import * as c from "./class.js"; // import class Tar
-// Tar Editor: main file.
-// Webpack packages this file.
+import {Tar} from "./class.js"; // import class Tar
+
+// _self is the global scope. it's determined by nested if statements.
+// below is a clarification.
 /*
-* Clarified logic for _self:
 * if window it's window
 * else
 **** if global it's global
@@ -11,10 +11,13 @@ import * as c from "./class.js"; // import class Tar
 ******** else
 ************ it's this
 */
+
 const _self = typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : this;
+
 const tar = {
 	init: (wrap, ln) => {
-		return new c.Tar(wrap, ln);
+		return new Tar(wrap, ln);
 	}
 };
+
 _self.tar = tar;
