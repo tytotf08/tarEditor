14e9781be1519fd8be8248cf4056359d4b21357f
tytotf08 <joshleeliu@gmail.com>
2020-12-22 13:55:26 -0500

    Annotate index.js.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import * as c from "./class.js";
+import * as c from "./class.js"; // import class Tar
+// Tar Editor: main file.
+// Webpack packages this file.
+/*
+* Clarified logic for _self:
+* if window it's window
+* else
+**** if global it's global
+**** else
+******** if self it's self
+******** else
+************ it's this
+*/
 const _self = typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : this;
 const tar = {
 	init: (wrap, ln) => {
